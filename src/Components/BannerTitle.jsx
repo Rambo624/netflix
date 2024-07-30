5e42a9d30a82b29528d92dcc8f0dff25d0d62ef4
Your Name <your_email@example.com>
2024-07-30 11:25:22 +0530

    added banner buttons

--- src/Components/BannerTitle.jsx
+++ src/Components/BannerTitle.jsx
@@ -6,6 +6,10 @@ function BannerTitle(props) {
     <div className="pt-[24%] w-screen h-[110%] absolute text-white pl-14 bg-gradient-to-r from-black">
         <h1 className='font-bold  text-6xl'>{title}</h1>
         <h2 className='text-xl w-[30%]' >{overview}</h2>
+        <div className='mt-4'>
+          <button className='bg-white text-black p-3 font-bold rounded-lg px-7 mr-3'>▶ Play</button>
+          <button className='bg-gray-500 text-white p-3 font-bold rounded-lg px-7'>More Info</button>
+        </div>
     </div>
   )
 }
