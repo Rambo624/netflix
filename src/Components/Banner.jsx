a872be321f439c8a1fb601daffd7506eae1f7233
Your Name <your_email@example.com>
2024-08-28 15:03:57 +0530

    removed random video

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,20 +1,15 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { useMemo } from 'react'
 import BannerTitle from './BannerTitle'
 import BannerVideo from './BannerVideo'
 function Banner() {
 
     const movies=useSelector(Store=>Store.movies?.nowPlayingMovies)
-    if(movies===null) return <h1>Loading....</h1>
+    if(movies===null) return
  
-    const mainMovie = useMemo(() => {
-      if (!movies || movies.length === 0) return null;
-      const randomIndex = Math.floor(Math.random() * movies.length);
-      return movies[randomIndex];
-    }, [movies]);
   
-    if (!mainMovie) return null;
+const mainMovie=movies[0]
+
 
   return (
     <div>
